fcd2bde285a87dcd6a73d8857ce8bd309414b846
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-06-04 21:52:28 +0300

    production security improvements

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const hpp = require("hpp");
+
 dotenv.config();
 
-const configRoutes = require('./src/routes/configRoutes');
-const authRoutes = require('./src/routes/authRoutes')
+app.use(helmet());
 
+const configRoutes = require("./src/routes/configRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(authRoutes) ;
+app.use(authRoutes);
 app.use(configRoutes);
+app.use(hpp());
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
