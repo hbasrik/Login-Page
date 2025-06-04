72ecc98519807b2f9fee1c3f5cccd24c5734d070
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-06-04 21:53:00 +0300

    production improvements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   base: './',
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    mangle: {
+      toplevel: true,
+    },
+  },
 })
